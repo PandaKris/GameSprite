9eb534de2f2b6d32c86d476c311fa89a0ab48da9
Kristanto Sean <kristantosean@gmail.com>
2025-06-19 14:47:55 +0700

    Fitur nambah kotak terus jatuh done!

--- GameSprite/ContentView.swift
+++ GameSprite/ContentView.swift
@@ -6,16 +6,40 @@
 //
 
 import SwiftUI
+import SpriteKit
 
+// A simple game scene with falling boxes
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+        box.position = location
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+        addChild(box)
+    }
+}
+
+// A sample SwiftUI creating a GameScene and sizing it
+// at 300x400 points
 struct ContentView: View {
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(
+            width: UIScreen.main.bounds.width,
+            height: UIScreen.main.bounds.height
+        )
+        scene.scaleMode = .fill
+        return scene
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        SpriteView(scene: scene)
+            .ignoresSafeArea()
     }
 }
 
